30de5ce4c0ce9b475586e36caf188ab47597557a
startfromC <1739329122@qq.com>
2024-07-05 23:35:52 +0800

    完成裁判录入界面的基本设计
    仅完成外形设计，点击事件等逻辑未处理

---
+++ pages/scoreAndCardsInput/scoreAndCardsInput.js
@@ -0,0 +1,95 @@
+// pages/scoreAndCardsInput/scoreAndCardsInput.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     match:'xx杯xxx场比赛',
+     scoreA: 3,
+     scoreB: 1,
+     teamA:'队伍A',
+     teamB:'队伍B',
+     teamAid:1,
+     teamBid:2,
+     players:[
+       {teamID:1,playerID:1},
+       {teamID:1,playerID:2},
+       {teamID:1,playerID:3},
+       {teamID:1,playerID:4},
+       {teamID:1,playerID:5},
+       {teamID:1,playerID:6},
+       {teamID:1,playerID:7},
+       {teamID:1,playerID:8},
+       {teamID:1,playerID:9},
+       {teamID:1,playerID:10},
+       {teamID:1,playerID:11},
+       {teamID:2,playerID:1},
+       {teamID:2,playerID:2},
+       {teamID:2,playerID:3},
+       {teamID:2,playerID:4},
+       {teamID:2,playerID:5},
+       {teamID:2,playerID:6},
+       {teamID:2,playerID:7},
+       {teamID:2,playerID:8},
+       {teamID:2,playerID:9},
+       {teamID:2,playerID:10},
+       {teamID:2,playerID:11}
+     ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    
+  },
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})

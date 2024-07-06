5c544ed55cb5803fd89c9ba0e3b7afd952735c91
Langqing Cui <langqing.cui@outlook.com>
2024-07-06 18:13:26 +0800

    完成选择账户

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,15 +1,22 @@
+const app = getApp()
+
 Page({
   data: {
     userInfo: {
       avatarUrl: '/image/page_me/default_avatar.png', // 默认头像
       nickName: '用户昵称',
-      role: '角色'
+      role: ''
     }
   },
 
   onLoad: function() {
-    this.getUserInfo();
+		this.getUserInfo();
+		this.setData({ userRole: app.globalData.userRole })
   },
+
+	onShow: function() {
+		this.setData({ userRole: app.globalData.userRole })
+	},
 
   getUserInfo: function() {
 

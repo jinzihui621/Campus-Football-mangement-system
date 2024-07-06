23fed8aa6966931a76b7e11d903f790b0c2bb4b2
Langqing Cui <langqing.cui@outlook.com>
2024-07-06 17:08:40 +0800

    实现跳转到chooseAccount

--- pages/me/me.js
+++ pages/me/me.js
@@ -12,27 +12,12 @@ Page({
   },
 
   getUserInfo: function() {
-    var that = this;
-    // wx.request({
-    //   url: 'https://your-backend-api.com/getUserInfo', // 替换为你的后端接口地址
-    //   method: 'GET',
-    //   success: function(res) {
-    //     if (res.statusCode === 200) {
-    //       let userInfo = res.data;
-    //       // 如果用户没有设置头像，使用默认头像
-    //       if (!userInfo.avatarUrl) {
-    //         userInfo.avatarUrl = '/image/default_avatar.png';
-    //       }
-    //       that.setData({
-    //         userInfo: userInfo
-    //       });
-    //     } else {
-    //       console.error('获取数据失败', res);
-    //     }
-    //   },
-    //   fail: function(err) {
-    //     console.error('请求失败', err);
-    //   }
-    // });
+
+	},
+	
+  navigateToChooseAccount: function() {
+    wx.navigateTo({
+      url: '/pages/chooseAccount/chooseAccount'
+    });
   }
 });

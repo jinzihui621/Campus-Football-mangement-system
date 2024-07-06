6c0c466bbd076b3cccdc659ede71ea5f31393a27
Langqing Cui <langqing.cui@outlook.com>
2024-07-06 15:23:44 +0800

    添加homePage接口具体信息

--- pages/homePage/homePage.js
+++ pages/homePage/homePage.js
@@ -2,6 +2,16 @@
 Page({
   data: {
 		matches: []  // 存储比赛信息
+		// 需要的信息示例如下：
+		// "id": 1,   -->比赛id
+		// "date": "2024-07-03",
+		// "time": "12:00",
+		// "game_running_flag": "1"   -->比赛是否在进行
+		// "location": "北操",
+		// "teamA": "信息A",
+		// "teamB": "信息B",
+		// "scoreA": 3,
+		// "scoreB": 1
   },
 
   onLoad(options) {

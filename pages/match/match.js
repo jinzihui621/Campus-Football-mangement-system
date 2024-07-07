a85b86cfff02bac9a8f1714491576648839e7d49
Langqing Cui <langqing.cui@outlook.com>
2024-07-07 22:18:05 +0800

    完成了match页面clq和zhl的合并

--- pages/match/match.js
+++ pages/match/match.js
@@ -14,22 +14,77 @@ Page({
     listType:['积分榜','球员榜','赛程'],
     listNumber:3,
     currentIndex: 0,
-    currentListIndex:2,
+		currentListIndex:0,
+		currentPlayerTab:'goals',
     date:'2024',
     turn:1, //当前显示的赛程轮次
     tableColumns: [
-      {title: "日期",key: "date",}, 
+      {title: "日期",key: "date",width:"100rpx"}, 
       {title: "队伍A",key: "teamA",}, 
       {title: "A",key: "teamAScore",},
       {title:"B",key:"teamBScore"},
       {title:"队伍B",key:"teamB"}
     ],
     list:[
-      {date:1,teamA:"type1",teamAScore:"-",teamBScore:'-',teamB:"name1"},
+      {date:"07-07 00:00",teamA:"type1",teamAScore:"-",teamBScore:'-',teamB:"name1"},
       {date:2,teamA:"type2",teamAScore:"-",teamBScore:'-',teamB:"name2"},
       {date:3,teamA:"type3",teamAScore:"3",teamBScore:'1',teamB:"name3"},
       {date:4,teamA:"type4",teamAScore:"2",teamBScore:'3',teamB:"name4"}
-    ],
+		],
+		tableRankColumns: [
+			{title: "名次", key: "rank", width: "80rpx"},
+			{title: "球队", key: "team", width: "150rpx"},
+			{title: "赛", key: "gamePlayed", width: "80rpx"},
+			{title: "胜", key: "win", width: "80rpx"},
+			{title: "平", key: "draw", width: "80rpx"},
+			{title: "负", key: "lose", width: "80rpx"},
+			{title: "进/失", key: "gs_ga", width: "120rpx"},
+			{title: "积分", key: "points", width: "80rpx"},
+		],
+		listRank: [
+			{rank:1,team:"计算机",gamePlayed:8,win:8,draw:0,lose:0,gs_ga:"35/2",points:24},
+			{rank:2,team:"城建",gamePlayed:8,win:7,draw:1,lose:0,gs_ga:"15/10",points:22}
+		],
+		tableGoalColumns: [
+			{title: "名次", key: "rank", width: "100rpx"},
+			{title: "球员", key: "player", width: "200rpx"},
+			{title: "球队", key: "team", width: "200rpx"},
+			{title: "进球数", key: "goalNum", width: "100rpx"},
+		],
+		listGoal: [
+			{rank:1,player:"playerA",team:"teamA",goalNum:15},
+			{rank:2,player:"playerB",team:"teamB",goalNum:12},
+			{rank:3,player:"playerC",team:"teamC",goalNum:10}
+		],
+		tableAssistColumns: [
+			{title: "名次", key: "rank", width: "100rpx"},
+			{title: "球员", key: "player", width: "200rpx"},
+			{title: "球队", key: "team", width: "200rpx"},
+			{title: "助攻数", key: "assistNum", width: "100rpx"},
+		],
+		listAssist: [
+			{rank:1,player:"playerD",team:"teamD",assistNum:7},
+			{rank:2,player:"playerE",team:"teamE",assistNum:5},
+			{rank:3,player:"playerF",team:"teamF",assistNum:4}
+		],
+		tableYellowCardColumns: [
+			{title: "名次", key: "rank", width: "100rpx"},
+			{title: "球员", key: "player", width: "200rpx"},
+			{title: "球队", key: "team", width: "200rpx"},
+			{title: "黄牌数", key: "yellowCardNum", width: "100rpx"},
+		],
+		listYellowCard: [
+			{rank:1,player:"playerF",team:"teamF",yellowCardNum:3},
+			{rank:2,player:"playerG",team:"teamG",yellowCardNum:1},
+		],
+		tableRedCardColumns: [
+			{title: "名次", key: "rank", width: "100rpx"},
+			{title: "球员", key: "player", width: "200rpx"},
+			{title: "球队", key: "team", width: "200rpx"},
+			{title: "红牌数", key: "redCardNum", width: "100rpx"},
+		],
+		listRedCard: [],
+
     "firstList": [{ name: 'w券1', money: '5.00' }, { name: 'w券2', money: '50.00'}],
     "secondList": [{ name: 'y券1', money: '10.00' }, { name: 'y券2', money: '20.00' }],
     "thirdList": [{ name: 'g券1', money: '30.00' }, { name: 'g券2', money: '40.00' }],
@@ -86,6 +141,12 @@ Page({
     this.setData({
       turn:e.currentTarget.dataset.current
     })
+	},
+	// 切换球员榜榜单
+	selectPlayerTab: function(e) {
+    this.setData({
+      currentPlayerTab: e.currentTarget.dataset.tab
+    });
   },
   /**
    * 生命周期函数--监听页面加载

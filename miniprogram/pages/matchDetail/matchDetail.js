e2786d41241265fa981c93d24cd540558635ebb4
Langqing Cui <langqing.cui@outlook.com>
2024-07-13 22:16:55 +0800

    更改matchDetail数据显示

--- miniprogram/pages/matchDetail/matchDetail.js
+++ miniprogram/pages/matchDetail/matchDetail.js
@@ -1,13 +1,9 @@
 Page({
   data: {
     events: [
-      { time: "22'", description: "金子辉进球", team: "teamA" },
-      { time: "32'", description: "谢锋红牌", team: "teamB" },
-      { time: "51'", description: "梅西进球", team: "teamA" },
-      { time: "55'", description: "梅西黄牌", team: "teamA" },
-      { time: "63'", description: "崔朗清进球", team: "teamB" },
-      { time: "64'", description: "徐鑫进球", team: "teamA" },
-      { time: "64'", description: "章鸿亮进球", team: "teamB" }
+			{ name: "金子辉", type: "进球", number: "2", team: "teamA"},
+			{ name: "金子辉", type: "黄牌", number: "1", team: "teamA"},
+			{ name: "徐鑫", type: "黄牌", number: "1", team: "teamB"}
     ],
     game_running_flag: 0,
     game_finished_flag: 0
